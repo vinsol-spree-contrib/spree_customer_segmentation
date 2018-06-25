ed0587ca15cb5696297df321eacdd9ad784c7a78
Shivam Jain <shivam@vinsol.com>
2018-06-25 12:05:44 +0530

    remove commented code, unneccesary code

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,12 +1,5 @@
 Spree::User.class_eval do
 
-  # scope :orders_revenue, -> { joins(:orders).merge(Spree::Order.revenue) }
-  #
-  # self.whitelisted_ransackable_scopes = %w(orders_revenue)
-
   delegate :full_name, :address, :phone, to: :bill_address, allow_nil: true
-
-
-  self.whitelisted_ransackable_associations += %w(orders)
 
 end
