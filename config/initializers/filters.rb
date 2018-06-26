af1ccca59e2631134a27e56f0a1215f01a919df2
Shivam Jain <shivam@vinsol.com>
2018-06-26 19:59:13 +0530

    update constants, add average order quantity filter

--- config/initializers/filters.rb
+++ config/initializers/filters.rb
@@ -1,6 +1,6 @@
-alphanumeric = { includes: 'includes', not_includes: 'not includes', contains: 'contains', does_not_contain: 'does not contain' }
-alphanumeric2 = { starts_with: 'starts with', includes: 'includes', not_includes: 'not includes' }
-alphanumeric3 = { contains: 'contains', does_not_contain: 'does not contain' }
+APLHANUMERIC = { includes: 'includes', not_includes: 'not includes', contains: 'contains', does_not_contain: 'does not contain' }
+APLHANUMERIC2 = { starts_with: 'starts with', includes: 'includes', not_includes: 'not includes' }
+APLHANUMERIC3 = { contains: 'contains', does_not_contain: 'does not contain' }
 
 NUMERIC_OPERATORS = { gt_eq: '>=', gt: '>', between: 'between', eq: '=', not_eq: '!=', lt: '<', lt_eq: '<=' }
 NUMERIC_OPERATORS_WITH_BLANK = { gt_eq: '>=', gt: '>', between: 'between', eq: '=', not_eq: '!=', lt: '<', lt_eq: '<=', blank: 'blank' }
@@ -8,9 +8,9 @@ NUMERIC_OPERATORS_WITH_BLANK = { gt_eq: '>=', gt: '>', between: 'between', eq: '
 
 FILTERS= {
   user: {
-    email: { metric_type: 'alphanumeric', operators: alphanumeric },
-    name: { metric_type: 'alphanumeric', operators: alphanumeric2 },
-    contact_number: { metric_type: 'alphanumeric', operators: alphanumeric3 }
+    email: { metric_type: 'alphanumeric', operators: APLHANUMERIC },
+    name: { metric_type: 'alphanumeric', operators: APLHANUMERIC2 },
+    contact_number: { metric_type: 'alphanumeric', operators: APLHANUMERIC3 }
   },
 
   order: {
@@ -19,6 +19,8 @@ FILTERS= {
     days_from_last_order: { metric_type: 'numeric', operators: NUMERIC_OPERATORS_WITH_BLANK },
     quantity_total: { metric_type: 'numeric', operators: NUMERIC_OPERATORS },
     total_number_of_order: { metric_type: 'numeric', operators: NUMERIC_OPERATORS },
+    quantity_average: { metric_type: 'numeric', operators: NUMERIC_OPERATORS },
+    frequency: { metric_type: 'numeric', operators: NUMERIC_OPERATORS },
   },
 
   session: {
--- config/initializers/search_service_mapper.rb
+++ config/initializers/search_service_mapper.rb
@@ -7,6 +7,8 @@ SEARCH_SERVICE_MAPPER = {
   order_days_from_first_order: Spree::CustomerSegmentation::Order::DaysFromFirstOrderFilterService,
   order_days_from_last_order: Spree::CustomerSegmentation::Order::DaysFromLastOrderFilterService,
   order_quantity_total: Spree::CustomerSegmentation::Order::QuantityTotalFilterService,
-  order_total_number_of_order: Spree::CustomerSegmentation::Order::TotalNumberOfOrderFilterService
+  order_total_number_of_order: Spree::CustomerSegmentation::Order::TotalNumberOfOrderFilterService,
+  order_quantity_average: Spree::CustomerSegmentation::Order::QuantityAverageFilterService,
+  order_frequency: Spree::CustomerSegmentation::Order::FrequencyFilterService,
 
 }
