70ea7e84e880522345dbffb486dabac989bf3d3f
Shivam Jain <shivam@vinsol.com>
2018-06-26 20:01:35 +0530

    remove commented scope

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,7 +2,6 @@ Spree::User.class_eval do
 
   delegate :full_name, :address, :phone, to: :bill_address, allow_nil: true
 
-  # scope :without_orders, -> { includes(:orders).where(spree_orders: {user_id: nil}) }
   scope :with_complete_orders, -> { joins(:orders).merge(Spree::Order.complete) }
   scope :without_complete_orders, -> { where.not(id: with_complete_orders.pluck(:id)) }
 
