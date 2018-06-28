66de453dfd16a30da658239768f92508b4d7d0c0
Pankaj Verma <pankaj@vinsol.com>
2018-06-28 15:44:40 +0530

    front end work added

--- app/assets/javascripts/spree/backend/spree_customer_segmentation.js
+++ app/assets/javascripts/spree/backend/spree_customer_segmentation.js
@@ -1,3 +1,4 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js
 //= require spree/backend/customer_segmentation
+//= require spree/backend/filter
