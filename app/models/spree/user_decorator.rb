ce9f295a1d7cc9be06700b34474f165f3a8d2951
Shivam Jain <shivam@vinsol.com>
2018-06-28 08:40:57 +0530

    add session filters

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -8,6 +8,10 @@ Spree::User.class_eval do
     Arel.sql('date(last_sign_in_at)')
   end
 
+  ransacker :last_active_session, type: :date do |parent|
+    Arel.sql('date(last_sign_in_at)')
+  end
+
   self.whitelisted_ransackable_associations += %w(orders)
   self.whitelisted_ransackable_attributes += %w(sign_in_count last_active_session)
 
