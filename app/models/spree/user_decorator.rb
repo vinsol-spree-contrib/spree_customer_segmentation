c246bb4ccd07fbbbec0f7cf845504d3a7a7f1b66
Shivam Jain <shivam@vinsol.com>
2018-06-29 11:06:29 +0530

    align scopes

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,7 +2,7 @@ Spree::User.class_eval do
 
   delegate :firstname, :lastname, :address, :phone, to: :bill_address, allow_nil: true
 
-  scope :with_complete_orders, -> { joins(:orders).merge(Spree::Order.complete).distinct }
+  scope :with_complete_orders,    -> { joins(:orders).merge(Spree::Order.complete).distinct }
   scope :without_complete_orders, -> { where.not(id: with_complete_orders.pluck(:id)) }
 
   self.whitelisted_ransackable_associations += %w(orders)
