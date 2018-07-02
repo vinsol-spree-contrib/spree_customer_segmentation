39fc3f8b3116714564b84ffb257397721fa946a3
Shivam Jain <shivam@vinsol.com>
2018-07-02 12:16:37 +0530

    rename address to full_address

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,6 +1,6 @@
 Spree::User.class_eval do
 
-  delegate :firstname, :lastname, :address, :phone, to: :bill_address, allow_nil: true
+  delegate :firstname, :lastname, :full_address, :phone, to: :bill_address, allow_nil: true
 
   scope :with_complete_orders,    -> { joins(:orders).merge(Spree::Order.complete).distinct }
   scope :without_complete_orders, -> { where.not(id: with_complete_orders.pluck(:id)) }
