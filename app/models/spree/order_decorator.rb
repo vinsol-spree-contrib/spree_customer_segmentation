f62727cfb9f6081ac8759b680b40c36916c3e118
usamaali201456 <usamaali201456@gmail.com>
2022-12-07 17:47:51 +0500

    Upgarding the ruby version in spree

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,9 +1,26 @@
-Spree::Order.class_eval do
+# Spree::Order.class_eval do
 
-  self.whitelisted_ransackable_attributes += %w(created_at)
+#   self.whitelisted_ransackable_attributes += %w(created_at)
 
-  ransacker :created_at, type: :date do
-    Arel.sql('date(created_at)')
+#   ransacker :created_at, type: :date do
+#     Arel.sql('date(created_at)')
+#   end
+
+# end
+
+module Spree
+  module OrderDecorator
+    def self.prepended(base)
+      base.whitelisted_ransackable_attributes += %w(created_at)
+
+      base.ransacker :created_at, type: :date do
+        Arel.sql('date(created_at)')
+      end
+    end
+    
   end
-
 end
+
+::Spree::Order.prepend Spree::OrderDecorator if ::Spree::Order.included_modules.exclude?(Spree::OrderDecorator)
+
+
--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,20 +1,47 @@
-Spree::User.class_eval do
+# Spree::User.class_eval do
 
-  include Spree::UserScopes
+#   include Spree::UserScopes
 
-  delegate :firstname, :lastname, :phone, to: :bill_address, allow_nil: true
+#   delegate :firstname, :lastname, :phone, to: :bill_address, allow_nil: true
 
-  has_many :customer_segments, class_name: 'Spree::CustomerSegment', foreign_key: :user_id, dependent: :destroy
+#   has_many :customer_segments, class_name: 'Spree::CustomerSegment', foreign_key: :user_id, dependent: :destroy
 
-  ransacker :last_active_session, type: :date do |parent|
-    Arel.sql('date(current_sign_in_at)')
+#   ransacker :last_active_session, type: :date do |parent|
+#     Arel.sql('date(current_sign_in_at)')
+#   end
+
+#   self.whitelisted_ransackable_associations += %w(orders)
+#   self.whitelisted_ransackable_attributes += %w(sign_in_count last_active_session)
+
+#   def user_presenter
+#     @user_presenter ||= ::Spree::CustomerSegmentation::UserPresenter.new(self)
+#   end
+
+# end
+
+
+module Spree
+  module UserDecorator
+    def self.prepended(base)     
+      base.include Spree::UserScopes
+
+      base.delegate :firstname, :lastname, :phone, to: :bill_address, allow_nil: true
+
+      base.has_many :customer_segments, class_name: 'Spree::CustomerSegment', foreign_key: :user_id, dependent: :destroy
+
+      base.ransacker :last_active_session, type: :date do |parent|
+        Arel.sql('date(current_sign_in_at)')
+      end
+
+      base.whitelisted_ransackable_associations += %w(orders)
+      base.whitelisted_ransackable_attributes += %w(sign_in_count last_active_session)
+
+      def user_presenter
+        @user_presenter ||= ::Spree::CustomerSegmentation::UserPresenter.new(base)
+      end
+    end 
   end
-
-  self.whitelisted_ransackable_associations += %w(orders)
-  self.whitelisted_ransackable_attributes += %w(sign_in_count last_active_session)
-
-  def user_presenter
-    @user_presenter ||= ::Spree::CustomerSegmentation::UserPresenter.new(self)
-  end
-
 end
+
+::Spree::User.prepend Spree::UserDecorator if ::Spree::User.included_modules.exclude?(Spree::UserDecorator)
+
